05160af376066099650445832b192aa743a585c6
Felix A. Schultz <felix.schultz@engapho.com>
2024-06-02 20:43:11 +0200

    feat: Add GDPR Cookiebanner plugin files and initial implementation
    This commit adds the necessary files for the GDPR Cookiebanner plugin version 3.2.0. It includes the index.php file, readme.txt updates, and new PHP files for the admin panel header and footer. Additionally, the intastellar-gdpr-cookiebanner.php file is included with the initial implementation of the plugin, including the enqueueing of the intastellar-gdpr-settings script.

---
+++ tags/3.2.0/includes/intastellarGDPRAdminPanelFooter.php
@@ -0,0 +1,9 @@
+<?php
+    _e(
+        '
+        <footer class="intastellarPoweredByLogoContainer">
+            <p>&copy; <?php echo date("Y") ?> Intastellar Solutions, International. All rights reserved.</p>
+        </footer>
+        '
+    );
+?>
--- tags/3.2.0/readme.txt
+++ tags/3.2.0/readme.txt
@@ -1,4 +1,4 @@
-GPDR Cookie banner, by Intastellar Solutions, International
+Intastellar Consents Solutions, by Intastellar Solutions, International
 
 License:            GPL v2 or later
 License URI:        https://www.gnu.org/licenses/gpl-2.0.html
